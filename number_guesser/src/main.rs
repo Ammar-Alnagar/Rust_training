a484d61b5c44fb22819c838e52a214c9557f5897
Ammar-Alnagar <ammaralnagar416@gmail.com>
2025-03-09 00:32:44 +0200

    wip

--- number_guesser/src/main.rs
+++ number_guesser/src/main.rs
@@ -1,19 +1,19 @@
 use std::{cmp::Ordering, io};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    let secret_number = 5;
 
+    let secret_number = rand::rng().random_range(1..=100);
     loop {
         println!("Please input your guess.");
 
     let mut guess = String::new();
 
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
-        println!("You guessed: {}", guess.trim());
 
     let guess: u32 = match guess.trim().parse() {
         Ok(num) => num,
@@ -23,12 +23,12 @@ fn main() {
         }
     };
 
-
+        println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
+        Ordering::Equal => {
                 println!("You win!");
                 break;
     }
